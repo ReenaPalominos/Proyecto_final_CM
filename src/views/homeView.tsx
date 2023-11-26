d5e1864c0ed6194d18838da02e9b175a12b6f6a4
Meshdako <msobarzo@utem.cl>
2023-11-26 20:46:29 -0300

    UPDATE: Página de Home funcionando. Un simple texto que confirma que el Login fue correcto

---
+++ src/views/homeView.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+
+export default function Home() {
+    
+
+    return (
+        <View>
+            <Text>Open up App.tsx to start working on your app!</Text>
+        </View>
+    );
+}

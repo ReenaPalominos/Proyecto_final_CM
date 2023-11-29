e2dd27828f8c46629d07d96ecaf58093f070bdef
Meshdako <msobarzo@utem.cl>
2023-11-29 20:37:36 -0300

    UPDATE: Vista de Login actualizada, pero dejada de lado para hacer funcionar las demás vistas. Posteriormente será retomada

--- src/views/loginView.tsx
+++ src/views/loginView.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Button, TextInput } from 'react-native-paper';
-import { View } from 'react-native';
+import { Pressable, StyleSheet, View, Text } from 'react-native';
 
 export default function Login({ navigation }) {
     const [username, setUsername] = useState<string>('');
@@ -16,21 +16,69 @@ export default function Login({ navigation }) {
     };
 
     return (
-        <View>
-            <TextInput
+        <View style={styles.loginView}>
+            <View style={styles.cardView}>
+                <TextInput
+                style={styles.userView}
                 label="Nombre de usuario"
                 value={username}
                 onChangeText={text => setUsername(text)}
-            />
-            <TextInput
+                />
+            
+                <TextInput
+                style={styles.passView}
                 label="Contraseña"
                 value={password}
                 onChangeText={text => setPassword(text)}
                 secureTextEntry
-            />
-            <Button onPress={handleLogin}>
-                Iniciar sesión
-            </Button>
+                />
+                <Pressable style={styles.buttonView} onPress={handleLogin}>
+                    <Text>Iniciar sesión</Text>
+                </Pressable>
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    loginView: {
+        flex: 1,
+        width: 'auto',
+        backgroundColor: '#ebebd3',
+
+        textAlign: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    cardView: {
+        alignItems: 'center',
+        justifyContent: 'center',
+
+        backgroundColor: '#000',
+        borderRadius: 10,
+        flex: 1,
+        width: 400,
+        margin: 50,
+    },
+    userView: {
+        width: 250,
+        height: 50,
+        margin: 10,
+        borderRadius: 10,
+    },
+    passView: {
+        width: 250,
+        height: 50,
+        margin: 10,
+        borderRadius: 10,
+    },
+    buttonView: {
+        width: 250,
+        height: 50,
+        margin: 10,
+        borderRadius: 10,
+        backgroundColor: '#ebebd3',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});

d7cf0d20d439df57a585cbd6ad5577dfe5454261
Meshdako <msobarzo@utem.cl>
2023-11-29 20:42:44 -0300

    UPDATE: Home muestra las el Stack

--- src/views/homeView.tsx
+++ src/views/homeView.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 
+import MyTabs from '../navigators/NavBar';
+
 export default function Home() {
     
 
     return (
-        <View>
-            <Text>Open up App.tsx to start working on your app!</Text>
-        </View>
+        <MyTabs />
     );
 }

ab0a48585e6116cf8d1a4b8186f1390d4e287b20
Meshdako <msobarzo@utem.cl>
2023-11-29 20:38:52 -0300

    UPDATE: Vista de Usuario creada

---
+++ src/views/usuarioView.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+
+export default function Usuario() {
+    
+
+    return (
+        <View>
+            <Text>Usuario</Text>
+        </View>
+    );
+}

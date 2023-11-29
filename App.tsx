acf60f1de0a1c2decefe3919b71425c09065c8b8
Meshdako <msobarzo@utem.cl>
2023-11-29 20:38:31 -0300

    UPDATE: Vista de Home puesta como principal para que la aplicación funcione sin problemas

--- App.tsx
+++ App.tsx
@@ -9,12 +9,7 @@ const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Home />
   );
 }
 

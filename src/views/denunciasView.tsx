5fe91b5fdad1f284ef8b8b0d1590b42578b6426e
Meshdako <msobarzo@utem.cl>
2023-11-29 20:37:01 -0300

    UPDATE: Vistas de Denuncias y Eventos creadas.

---
+++ src/views/denunciasView.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+
+export default function Denuncias() {
+    
+
+    return (
+        <View>
+            <Text>Denuncias</Text>
+        </View>
+    );
+}
---
+++ src/views/eventosView.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+
+export default function Eventos() {
+    
+
+    return (
+        <View>
+            <Text>Eventos</Text>
+        </View>
+    );
+}

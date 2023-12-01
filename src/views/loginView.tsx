63ba14203b6b8992bf29f4159fa59b9da4db5532
Meshdako <msobarzo@utem.cl>
2023-12-01 03:17:08 -0300

    UPDATE: Estilo visual de la vista de Login actualizada. Agregado un estilo Blur de fondo, al igual que a los input y el botón de Inicio de Sesión fue cambiado a uno más estiloso. Se corrigió el NAVIGATION para que este no fuera de tipo ANY implícito, se agregó el Props que se ve en la línea 13, el cual fue agregado en el NavBar como StackParamList

--- src/views/loginView.tsx
+++ src/views/loginView.tsx
@@ -1,16 +1,26 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, Button, TextInput, Alert } from 'react-native';
+import { StyleSheet, Image, View, Text, Button, TextInput, Alert, Pressable, SafeAreaView } from 'react-native';
+import { BlurView } from "expo-blur";
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { StackParamList } from '../navigators/NavBar';
 
 import appFirebase from '../services/firebase';
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
 
 const auth = getAuth(appFirebase);
 
-export default function Login({ navigation }) {
+type Props = {
+    navigation: NativeStackNavigationProp<StackParamList>;
+}
+
+export default function Login ({ navigation } : Props){
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [isButtonPressed, setIsButtonPressed] = useState(false);
 
     const loginApp = async () => { 
+        setIsButtonPressed(true);
         try {
             await signInWithEmailAndPassword(auth, email, password);
             Alert.alert('Iniciando Sesión','Accediendo...');
@@ -19,28 +29,91 @@ export default function Login({ navigation }) {
             console.log(error);
             Alert.alert('Error','Usuario o contraseña incorrectos');
         }
+        setIsButtonPressed(false);
     };
 
     
     return (
-        <View style={styles.container}>
-            <TextInput placeholder="Email" onChangeText={(text) => setEmail(text)} />
-            <TextInput placeholder="Password" onChangeText={(text) => setPassword(text)} secureTextEntry />
-            <Button title="Sign In"  onPress={loginApp} />
-        </View>
+        <SafeAreaView style={styles.container} >
+            <BlurView intensity={70} tint="dark" style={styles.blurContainer}>
+                <Image 
+                    style={styles.image}
+                    source={require("../../assets/icons/adaptive-icon.png")}
+                />
+                <TextInput style={styles.textInputContainer} placeholder="Email" onChangeText={(text) => setEmail(text)} />
+                <TextInput style={styles.textInputContainer} placeholder="Password" onChangeText={(text) => setPassword(text)} secureTextEntry />
+                <Pressable style={isButtonPressed ? styles.pressedButton : styles.button} onPress={loginApp}>
+                    <Text style={styles.text}>Sign In</Text>
+                </Pressable>
+            </BlurView>
+        </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+
         backgroundColor: '#55EBFF',
+
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    blurContainer: {
+        width: 400,
+        height: 500,
+
+        borderRadius: 20,
+
+        overflow: 'hidden',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    image: {
+        width: 100,
+        height: 100,
+        
+        marginBottom: 40,
+    },
+    textInputContainer: {
+        width: 300,
+        height: 50,
+        marginBottom: 20,
+        padding: 10,
+
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: 'white',
+
+        color: '#000',
+        backgroundColor: 'white',
+
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    button: {
+        width: 200,
+        padding: 15,
+        margin: 10,
+        alignItems: 'center',
+    
+        borderRadius: 5,
+
+        backgroundColor: '#00E0FF',
+    },
+    pressedButton: {
+        width: 200,
+        padding: 15,
+        margin: 10,
+        alignItems: 'center',
+    
+        borderRadius: 5,
+
+        backgroundColor: '#55EBFF',
     },
     text: {
         color: 'white',
         fontSize: 20,
         fontWeight: 'bold',
-    }
+    },
 });

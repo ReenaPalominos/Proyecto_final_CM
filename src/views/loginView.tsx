ac68e5616fd8a0fe659bc2156decaa36a23c9a53
Meshdako <msobarzo@utem.cl>
2023-11-30 21:43:14 -0300

    UPDATE: Login del Usuario configurado y funcionando para la conexión con la base de datos Firebase

--- src/views/loginView.tsx
+++ src/views/loginView.tsx
@@ -1,84 +1,46 @@
 import React, { useState } from 'react';
-import { Button, TextInput } from 'react-native-paper';
-import { Pressable, StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Button, TextInput, Alert } from 'react-native';
+
+import appFirebase from '../services/firebase';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth(appFirebase);
 
 export default function Login({ navigation }) {
-    const [username, setUsername] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    const handleLogin = () => {
-        // Aquí puedes manejar la lógica de inicio de sesión
-        if (username === 'mesh' && password === '1234') {
-            navigation.navigate('Home');
-        } else {
-            alert('Usuario incorrecto');
+    const loginApp = async () => { 
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+            Alert.alert('Iniciando Sesión','Accediendo...');
+            navigation.navigate('Vistas');
+        } catch (error) {
+            console.log(error);
+            Alert.alert('Error','Usuario o contraseña incorrectos');
         }
     };
 
+    
     return (
-        <View style={styles.loginView}>
-            <View style={styles.cardView}>
-                <TextInput
-                style={styles.userView}
-                label="Nombre de usuario"
-                value={username}
-                onChangeText={text => setUsername(text)}
-                />
-            
-                <TextInput
-                style={styles.passView}
-                label="Contraseña"
-                value={password}
-                onChangeText={text => setPassword(text)}
-                secureTextEntry
-                />
-                <Pressable style={styles.buttonView} onPress={handleLogin}>
-                    <Text>Iniciar sesión</Text>
-                </Pressable>
-            </View>
+        <View style={styles.container}>
+            <TextInput placeholder="Email" onChangeText={(text) => setEmail(text)} />
+            <TextInput placeholder="Password" onChangeText={(text) => setPassword(text)} secureTextEntry />
+            <Button title="Sign In"  onPress={loginApp} />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    loginView: {
+    container: {
         flex: 1,
-        width: 'auto',
-        backgroundColor: '#ebebd3',
-
-        textAlign: 'center',
+        backgroundColor: '#55EBFF',
         alignItems: 'center',
         justifyContent: 'center',
     },
-    cardView: {
-        alignItems: 'center',
-        justifyContent: 'center',
-
-        backgroundColor: '#000',
-        borderRadius: 10,
-        flex: 1,
-        width: 400,
-        margin: 50,
-    },
-    userView: {
-        width: 250,
-        height: 50,
-        margin: 10,
-        borderRadius: 10,
-    },
-    passView: {
-        width: 250,
-        height: 50,
-        margin: 10,
-        borderRadius: 10,
-    },
-    buttonView: {
-        width: 250,
-        height: 50,
-        margin: 10,
-        borderRadius: 10,
-        backgroundColor: '#ebebd3',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+    text: {
+        color: 'white',
+        fontSize: 20,
+        fontWeight: 'bold',
+    }
 });

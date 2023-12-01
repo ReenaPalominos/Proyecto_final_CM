b492c7ce387f162a0ba6414d1906f93f69728fcd
Meshdako <msobarzo@utem.cl>
2023-12-01 03:14:35 -0300

    UPDATE: Solo fueron borrados ciertos espacios

--- App.tsx
+++ App.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Login from './src/views/loginView';
 import Home from './src/views/homeView';
-
-
-const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <Home />
   );
 }
-

dd24857cfa3bd888530af49a63bf16501d532e86
Meshdako <msobarzo@utem.cl>
2023-12-07 13:23:17 -0300

    UPDATE

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from './src/views/homeView';
 
 import Maps from './src/views/MapView';
+import TestCam from './src/views/TEST';
 
 export default function App() {
   return (

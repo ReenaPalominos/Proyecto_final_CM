ac1a55bd205495194441bc753cd117400ebb847a
Meshdako <msobarzo@utem.cl>
2023-12-07 13:23:05 -0300

    UPDATE: Vista para Iniciar Sesión corregida para que cargue el componente de Inicio de Sesión.

--- src/views/loginView.tsx
+++ src/views/loginView.tsx
@@ -1,131 +1,18 @@
-import React, { useState } from 'react';
-import { StyleSheet, Image, View, Text, Button, TextInput, Alert, Pressable, SafeAreaView } from 'react-native';
-import { BlurView } from "expo-blur";
+import React from 'react';
+import { View } from 'react-native';
+import { LoginComponent } from '../components/loginComponent';
+
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { StackParamList } from '../navigators/NavBar';
-
-import appFirebase from '../services/firebaseConfig';
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-
-
-const auth = getAuth(appFirebase);
-
 type Props = {
     navigation: NativeStackNavigationProp<StackParamList>;
 }
 
-export default function Login ({ navigation } : Props){
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [isButtonPressed, setIsButtonPressed] = useState(false);
-
-    const loginApp = async () => { 
-        setIsButtonPressed(true);
-        try {
-            await signInWithEmailAndPassword(auth, email, password);
-            Alert.alert('Iniciando Sesión','Accediendo...');
-            navigation.navigate('Vistas');
-        } catch (error) {
-            console.log(error);
-            Alert.alert('Error','Usuario o contraseña incorrectos');
-        }
-        setIsButtonPressed(false);
-    };
-
-    
+export default function Login ({ navigation }: Props){
     return (
-        <SafeAreaView style={styles.container} >
-            <BlurView intensity={70} tint="dark" style={styles.blurContainer}>
-                <Image 
-                    style={styles.image}
-                    source={require("../../assets/icons/adaptive-icon.png")}
-                />
-                <Text style={styles.title}>Inicia Sesión</Text>
-                <TextInput style={styles.textInputContainer} placeholder="Ingresa tu email" onChangeText={(text) => setEmail(text)} />
-                <TextInput style={styles.textInputContainer} placeholder="Ingresa tu contraseña" onChangeText={(text) => setPassword(text)} secureTextEntry />
-                <Pressable style={isButtonPressed ? styles.pressedButton : styles.button} onPress={loginApp}>
-                    <Text style={isButtonPressed ? styles.pressedText : styles.text}>Sign In</Text>
-                </Pressable>
-            </BlurView>
-        </SafeAreaView>
+        <View>
+            <LoginComponent navigation={navigation} />
+        </View>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-
-        backgroundColor: '#55EBFF',
-
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    blurContainer: {
-        width: 400,
-        height: 500,
-
-        borderRadius: 20,
-
-        overflow: 'hidden',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    image: {
-        width: 100,
-        height: 100,
-        
-        marginBottom: 40,
-    },
-    textInputContainer: {
-        width: 300,
-        height: 50,
-        marginBottom: 20,
-        padding: 10,
-
-        borderRadius: 5,
-        borderWidth: 1,
-        borderColor: 'white',
-
-        color: '#000',
-        backgroundColor: 'white',
-
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    button: {
-        width: 200,
-        padding: 15,
-        margin: 10,
-        alignItems: 'center',
-    
-        borderRadius: 5,
-
-        backgroundColor: '#00E0FF',
-    },
-    pressedButton: {
-        width: 200,
-        padding: 15,
-        margin: 10,
-        alignItems: 'center',
-    
-        borderRadius: 5,
-
-        backgroundColor: '#E0FBFC',
-    },
-    title: {
-        marginBottom: 30,
-        fontSize: 26,
-        color: 'white',
-        fontWeight: 'bold',
-    },
-    text: {
-        color: 'white',
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-    pressedText: {
-        color: 'black',
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-});

da873f7af0f8fd173ac46c260e96147c66aa1357
Renato Palominos <rpalominos@utem.cl>
2023-12-08 17:53:21 -0300

    UPDATE: Se mejora el estilo

--- src/views/vistasView.tsx
+++ src/views/vistasView.tsx
@@ -1,137 +1,127 @@
 import React from 'react';
-import { View, Image, Text, Pressable, StyleSheet } from 'react-native';
+import { View, Image, Text, Pressable, StyleSheet, Dimensions } from 'react-native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { StackParamList } from '../navigators/NavBar';
+import { Ionicons } from '@expo/vector-icons'; // Importa los íconos desde @expo/vector-icons
 
-import appFirebase from '../services/firebaseConfig';
+import { appFirebase } from '../services/firebaseConfig';
 import { getAuth } from 'firebase/auth';
 
 const auth = getAuth(appFirebase);
+const { width, height } = Dimensions.get('window');
 
 type Props = {
-    navigation: NativeStackNavigationProp<StackParamList>;
+  navigation: NativeStackNavigationProp<StackParamList>;
+};
+
+export default function Vistas({ navigation }: Props) {
+  const userID = auth.currentUser;
+
+  const navigateToAboutUs = () => {
+    navigation.navigate('AboutUs');
+  };
+
+  const ButtonWithIcon = ({ onPress, text, iconName }: { onPress: () => void; text: string; iconName: string }) => (
+    <Pressable style={styles.button} onPress={onPress}>
+      <Ionicons name={iconName} size={40} color="#023e8a" />
+      <Text style={styles.buttonText}>{text}</Text>
+    </Pressable>
+  );
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.profileContainer}>
+        <Image source={require('../../assets/profilepic.png')} style={styles.userImage} />
+        <Text style={styles.text}>
+          {userID ? userID.email : 'No hay ningún usuario autenticado'}
+        </Text>
+      </View>
+      <View style={styles.subContainer}>
+        <View style={styles.buttonRow}>
+          <ButtonWithIcon onPress={() => navigation.navigate('Denuncias')} text="Denuncias" iconName="ios-alert-circle-outline" />
+          <ButtonWithIcon onPress={() => navigation.navigate('Eventos')} text="Eventos" iconName="ios-calendar-outline" />
+        </View>
+        <View style={styles.buttonRow}>
+          <ButtonWithIcon onPress={() => navigation.navigate('Mapa')} text="Mapa" iconName="ios-map-outline" />
+          <ButtonWithIcon onPress={() => navigation.navigate('Usuario')} text="Usuario" iconName="ios-person-outline" />
+        </View>
+      </View>
+      <Pressable style={styles.circularButton} onPress={navigateToAboutUs}>
+        <Text style={styles.text}>?</Text>
+      </Pressable>
+    </View>
+  );
 }
 
-export default function Vistas({ navigation } : Props) {
-
-    const userID = auth.currentUser;
-
-    const navigateToAboutUs = () => {
-        navigation.navigate('AboutUs');
-    };
-
-    return (
-        <View style={styles.container}>
-            <View style={styles.userContainer}>
-                <Image
-                    source={require('../../assets/profilepic.png')}
-                    style={styles.userImage}
-                />
-                <Text style={styles.text}>
-                    {userID ? userID.email : "No hay ningún usuario autenticado"}
-                </Text>
-            </View>
-            <View style={styles.subContainer}>
-                <Pressable
-                style={styles.button}
-                onPress={() => navigation.navigate('Denuncias')} >
-                    <Text 
-                        style={styles.text} >
-                        Denuncias
-                    </Text>
-                </Pressable>
-        
-                <Pressable 
-                style={styles.button} 
-                onPress={() => navigation.navigate('Eventos')} >
-                    <Text 
-                        style={styles.text} >
-                        Eventos
-                    </Text>
-                </Pressable>
-                <Pressable 
-                style={styles.button} 
-                onPress={() => navigation.navigate('Mapa')} >
-                    <Text 
-                        style={styles.text} >
-                        Mapa
-                    </Text>
-                </Pressable>
-                <Pressable 
-                style={styles.button} 
-                onPress={() => navigation.navigate('Usuario')} >
-                    <Text 
-                        style={styles.text} >
-                        Usuario
-                    </Text>
-                </Pressable>
-            </View>
-            <Pressable
-                style={styles.circularButton}
-                onPress={navigateToAboutUs}
-            >
-                <Text style={styles.text}>?</Text>
-            </Pressable>
-        </View>
-        );
-    }
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#55EBFF',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    userImage:{
-        width: 100,
-        height: 100,
-        borderRadius: 100,
-    },
-    userContainer: {
-        width: 400,
-        height: 200,
-        marginBottom: 20,
-
-        borderRadius: 10,
-        backgroundColor: '#00E0FF',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    subContainer: {
-        width: 400,
-        height: 600,
-
-        borderRadius: 10,
-
-        backgroundColor: '#00E0FF',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    button: {
-        width: 200,
-        padding: 15,
-        margin: 10,
-        alignItems: 'center',
-    
-        borderRadius: 5,
-        borderWidth: 1,
-        borderColor: 'white',
-    },
-    text: {
-        color: 'white',
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-    circularButton: {
-        position: 'absolute',
-        bottom: 20,
-        right: 20,
-        width: 30,
-        height: 30,
-        borderRadius: 25,
-        backgroundColor: '#44E8FF',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    width: width,
+    height: height,
+    backgroundColor: '#90e0ef',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  profileContainer: {
+    width: width,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#90e0ef',
+    borderRadius: 10,
+    height: '10%',
+    marginTop: 40,
+  },
+  userImage: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    marginRight: 10,
+  },
+  subContainer: {
+    width: width,
+    height: '70%',
+    borderRadius: 10,
+    backgroundColor: '#90e0ef',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    width: 150,
+    height: 150,
+    margin: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    backgroundColor: '#48cae4',
+  },
+  buttonText: {
+    color: '#023e8a',
+    fontSize: 16,
+    marginTop: 5,
+    fontWeight: 'bold',
+  },
+  buttonRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    marginBottom: 20,
+  },
+  text: {
+    color: '#023e8a',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  circularButton: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    width: 30,
+    height: 30,
+    borderRadius: 25,
+    backgroundColor: '#44E8FF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });

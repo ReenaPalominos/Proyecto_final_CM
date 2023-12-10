496afa5b954557bbd9ecc98e571c61ea43b0f29d
Renato Palominos <rpalominos@utem.cl>
2023-12-10 17:42:32 -0300

    FIX: Posición inicial

--- src/components/GeneralMapComponent.tsx
+++ src/components/GeneralMapComponent.tsx
@@ -4,16 +4,21 @@ import { StyleSheet, View } from "react-native";
 import { getLocation } from "../services/location";
 
 import { Locations} from "../interfaces/location.interface";
+import Loading from "./Loading";
 
 interface IProps {
     location_array: Locations[];
 }
 
 export const GMapComponent : React.FC<IProps> = ( {location_array} ) => {
-    const [markers, setMarkers] = useState([{ latitude: 0, longitude: 0 }])
+    const [markers, setMarkers] = useState([{ latitude: 0, longitude: 0 }]);
     const [index, setIndex] = useState(0);
+    const [latitud, setLatitud] = useState(0);
+    const [longitud, setLongitud] = useState(0);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
+        setLoading(true);
         const newMarkers = location_array.map(location => ({
             latitude: Number(location.latitud),
             longitude: Number(location.longitud),
@@ -22,15 +27,40 @@ export const GMapComponent : React.FC<IProps> = ( {location_array} ) => {
         console.log(index, newMarkers);
         setMarkers(newMarkers);
         setIndex(index + 1);
+        setLoading(false);
     }, [location_array]);
+
+    const setubication = async () => {
+        setLoading(true);
+        let ubicacion = await getLocation();
+        if (ubicacion !== undefined) {
+          let [lat, lon] = ubicacion;
+          const parsedLat = Number(lat);
+          const parsedLon = Number(lon);
+          setLatitud(parsedLat);
+          setLongitud(parsedLon);
+          console.log("Latitud: " + parsedLat + " Longitud: " + parsedLon);
+          console.log("ubicacion: " + ubicacion);
+        }
+        setLoading(false); // Al terminar la carga de ubicación, cambiar a false
+      };
+    
+      useEffect(() => {
+        setLoading(true);
+        setubication();
+        setLoading(false);
+      }, []);
 
     return (
         <View style={styles.container}>
-            <MapView style={styles.map} provider={'google'} showsUserLocation showsMyLocationButton initialRegion={{latitude: 0, longitude: 0, latitudeDelta: 0.01, longitudeDelta: 0.01}}>
+            {loading ? (
+                <Loading /> ) : (
+            <MapView style={styles.map} provider={'google'} showsUserLocation showsMyLocationButton initialRegion={{latitude: latitud, longitude: longitud, latitudeDelta: 0.01, longitudeDelta: 0.01}}>
                 {markers.map((marker, index) => (
                     <Marker key={index} coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}></Marker>
                 ))}
             </MapView>
+                )}
         </View>
     )
 }

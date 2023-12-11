12c1a3add8f36e9f2483bec0133020b4b1f5b85e
Meshdako <msobarzo@utem.cl>
2023-12-10 21:27:54 -0300

    UPDATE: Actualizado la interface que sirve para consumir los elementos que se descargan y así mostrarlos en las distintas vistas

---
+++ src/interfaces/datos.interface.tsx
@@ -0,0 +1,10 @@
+export interface Datos {
+    token: string | number[];
+    timestamp: number;
+    title: string;
+    description: string;
+    file: string;
+    userId: string;
+    latitud: string;
+    longitud: string;
+}

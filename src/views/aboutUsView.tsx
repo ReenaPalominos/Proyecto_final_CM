d176b308a00dcccae22127bd693754b424ab2a36
Renato Palominos <rpalominos@utem.cl>
2023-12-11 00:12:16 -0300

    UPDATE: AboutUs

--- src/views/aboutUsView.tsx
+++ src/views/aboutUsView.tsx
@@ -7,9 +7,10 @@ export default function AboutUs(){
 
     return(
         <View style={styles.container}>
-            <Text>ABOUT US</Text>
+            <Text style={styles.title}>Sobre la Aplicación</Text>
             <Text style={styles.texto}>Esta aplicación fue desarrollada como trabajo final de la asignatura computación móvil
             de la Universidad Tecnológica Metropolitana de Santiago de Chile.</Text>
+            <Text style={styles.texto}>El proyecto consiste en una aplicación que permita al usuario publicar tanto Denuncias como Eventos Ecologicos.</Text>
             <Text style={styles.texto}>
                 Los estudiantes a cargo del desarrollo de esta aplicación son los siguientes:
             </Text>
@@ -43,6 +44,13 @@ const styles = StyleSheet.create({
         padding: 10,
         margin: 10,
     },
+    title:{
+        color: '#023e8a',
+        fontSize: 20,
+        marginTop: 5,
+        fontWeight: 'bold',
+
+    },
     texto:{
         color: '#023e8a',
         fontSize: 16,
@@ -50,8 +58,5 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
 
     },
-    integrante:{
-        
-    }
 });
 

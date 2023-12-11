8e47676c90c758967f5f8e25b8353bf50b49a980
Meshdako <msobarzo@utem.cl>
2023-12-10 21:25:41 -0300

    UPDATE: Unificada la vista de Añadir, para así poder tener una sola vista y no escribir dos veces el mismo código, pero con una línea distinta. Se actualizaron los componentes correspondientes para lo mismo.

---
+++ src/components/AddComponent.tsx
@@ -0,0 +1,160 @@
+import React, { useEffect, useState } from "react";
+import {
+    Button,
+    Dimensions,
+    Image,
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+    TouchableOpacity,
+    Platform,
+    Alert,
+    TextInput
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+/* FireBase */
+import { db, storage } from "../services/firebaseConfig";
+import { ref, uploadBytesResumable, getStorage, getDownloadURL } from 'firebase/storage';
+
+/* Para la subida de imágenes. */
+import * as ImagePicker from "expo-image-picker";
+import * as Progress from 'react-native-progress';
+import uuid from 'react-native-uuid';
+
+/* Components */
+import { GaleryComponent } from "./GaleryComponent";
+import { UploadComponent } from "./UploadComponent";
+import { FormComponent } from "./FormComponent";
+import { set } from "firebase/database";
+
+interface AddComponentProps {
+    tipo: string;
+}
+
+export const AddComponent: React.FC<AddComponentProps> = ({ tipo }) => {
+    const [image, setImage] = useState<string>("");
+    const [imageToken, setImageToken] = useState<string | number[]>("");
+
+    const [fileUpload, setFileUpload] = useState(false);
+
+    const [file, setFile] = useState<unknown>("");
+
+    const handleImage = (imageUrl: string) => {
+        setImage(imageUrl);
+    };
+
+    const handleUpload = (image: string, imageToken: string | number[], fileUpload: boolean, file: unknown) => {
+        console.log(" ImageToken: " + imageToken + " FileUpload: " + fileUpload + " File: " + file);
+        
+        setImage(image);
+        setImageToken(imageToken);
+        setFileUpload(fileUpload);
+        setFile(file);
+    };
+
+    return (
+        <SafeAreaView style={styles.container}>
+            {fileUpload ? (
+                <FormComponent
+                    tipo={tipo}
+                    token={imageToken}
+                    file={file}
+                />
+            ) : (
+                <>
+                    <GaleryComponent
+                        onImageSelected={handleImage}
+                    />
+                    
+                    <TouchableOpacity style={styles.cameraPicker} /* onPress={selectImage} */>
+                        <Ionicons name="camera" size={24} color="white" />
+                    </TouchableOpacity>
+                    
+                    <Text style={styles.titleContainer}>Subir imagen</Text>
+                    <Text style={styles.textContainer}>Selecciona una imagen de tu galería o captura una nueva.</Text>
+                    
+                    <View style={styles.imageContainer}>
+                        <UploadComponent
+                                tipo={tipo}
+                                image={image}
+                                onUploadUpdate={handleUpload}
+                            />
+                    </View>
+                </>
+            )}
+            
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    titleContainer: {
+        fontSize: 36,
+        marginTop: 20,
+        marginBottom: 20, 
+        textAlign: 'center'
+    },
+    textContainer: {
+        fontSize: 14,
+        marginTop: 20,
+        textAlign: 'center'
+    },
+    imageContainer: {
+        marginTop: 30,
+        marginBottom: 50,
+        alignItems: 'center'
+    },
+    imageBox: {
+        width: 450,
+        height: 300,
+
+        marginTop: 20,
+    },
+    progressBarContainer: {
+        marginTop: 20
+    },
+    uploadButton: {
+        borderRadius: 5,
+        width: 150,
+        height: 50,
+        backgroundColor: '#ffb6b9',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 20
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+    imagePicker: {
+        position: "absolute",
+        bottom: 90,
+        right: 30,
+        width: 45,
+        height: 45,
+        backgroundColor: "black",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 25,
+    },
+    cameraPicker: {
+        position: "absolute",
+        bottom: 30,
+        right: 30,
+        width: 45,
+        height: 45,
+        backgroundColor: "black",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 25,
+    },
+});
---
+++ src/components/UploadComponent.tsx
@@ -0,0 +1,145 @@
+import React, { useEffect, useState } from 'react';
+import { Alert, TouchableOpacity, View, Image, Text, StyleSheet } from 'react-native';
+import { storage } from "../services/firebaseConfig";
+import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+
+import * as Progress from 'react-native-progress';
+import uuid from 'react-native-uuid';
+
+interface IUploadComponentProps {
+    onUploadUpdate: (image: string, token: string | number[], fileUpload: boolean, file: unknown) => void;
+    tipo: string;
+    image: string;
+}
+
+export const UploadComponent = ({ tipo, image, onUploadUpdate }: IUploadComponentProps) => {   
+    const [uploading, setUploading] = useState(false);
+    const [transferred, setTransferred] = useState(0);
+    const [token, setToken] = useState<string | number[]>("");
+    const [pressed, setPressed] = useState(false);
+
+    const imageSelected = image;
+    const _token = uuid.v4();
+
+    const uploadImage = async() => {
+        const response = await fetch(image);
+        const blob = await response.blob();
+
+        return new Promise((resolve, reject) => {
+            
+            let storageRef;
+            ( tipo == "Evento" ? (
+                storageRef = ref(storage, "Evento/" + _token)
+            ) : (
+                storageRef = ref(storage, "Denuncias/" + _token)
+            ))
+            
+            const uploadTask = uploadBytesResumable(storageRef, blob);
+
+            setTransferred(0);
+
+            uploadTask.on(
+                "state_changed",
+                (snapshot) => {
+                    let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log("Progreso: " + progress + "%");
+                    setTransferred(progress);
+                },
+                (error) => {
+                    console.log("Error al subir la imagen: " + error.toString());
+                    reject(error);
+                },
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                        console.log("Archivo disponible en: " + downloadURL);
+                        resolve(downloadURL);
+                    });
+                },
+            );
+        });
+    };
+
+    return (
+        <View style={styles.imageContainer}>
+            {imageSelected !== "" ? (
+                <Image source={{ uri: imageSelected }} style={styles.imageBox} />
+            ) : ""}
+            
+            {uploading ? (
+                <View style={styles.progressBarContainer}>
+                    <Progress.Bar progress={transferred} width={300} />
+                </View>
+            ) : (
+            <TouchableOpacity 
+                style={ imageSelected != "" ?
+                    (pressed ? styles.pressButton : styles.pressableButton) 
+                        : (styles.uploadButton)
+                } 
+                onPress={ async () => {
+                    setPressed(true);
+                    setUploading(true);
+                    try {
+                        const response = await uploadImage();
+                        Alert.alert('Foto subida con éxito!');
+                        onUploadUpdate("", _token, true, response);
+                    } catch (error) {
+                        console.log("Error al subir la imagen: " + error);
+                    }
+                    setUploading(false);
+                    setPressed(false);
+                }}
+            >
+                <Text style={styles.buttonText}>Subir Imagen</Text>
+            </TouchableOpacity> )} 
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    progressBarContainer: {
+        marginTop: 20
+    },
+    pressableButton: {
+        borderRadius: 5,
+        width: 150,
+        height: 50,
+        backgroundColor: '#f53b42',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 20
+    },
+    pressButton: {
+        borderRadius: 5,
+        width: 150,
+        height: 50,
+        backgroundColor: '#ffb6b9',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 20
+    },
+    uploadButton: {
+        borderRadius: 5,
+        width: 150,
+        height: 50,
+        backgroundColor: '#ffb6b9',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 20
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+    imageContainer: {
+        marginTop: 30,
+        marginBottom: 50,
+        alignItems: 'center'
+    },
+    imageBox: {
+        width: 450,
+        height: 300,
+
+        marginTop: 20,
+    },
+});
